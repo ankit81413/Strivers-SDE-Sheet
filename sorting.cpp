5f7d3c09315142802f07bba2692ddc5d8555311e
Ankit <ankitsen81413@gmail.com>
2025-09-13 23:38:07 +0530

    bubble sort

--- sorting.cpp
+++ sorting.cpp
@@ -2,35 +2,54 @@
 #include <vector>
 using namespace std;
 
-vector<int> selectionsort(vector<int> arr){
-    int n = arr.size();
+// vector<int> selectionsort(vector<int> arr){
+//     int n = arr.size();
     
 
-    for(int i =0 ; i <= n-1; i++){
-        int min = i;
-        for(int j = i+1 ; j < n; j++){
-            if (arr[j]<arr[i]){
-                min = j;
-            }
+//     for(int i =0 ; i <= n-1; i++){
+//         int min = i;
+//         for(int j = i+1 ; j < n; j++){
+//             if (arr[j]<arr[i]){
+//                 min = j;
+//             }
 
-            if(min!= i){
-                swap(arr[i],arr[min]);
-            }
+//             if(min!= i){
+//                 swap(arr[i],arr[min]);
+//             }
             
-        }
-     }
+//         }
+//      }
 
+//     return arr;
+// }
+
+
+
+vector<int> bubblesort(vector<int> arr){
+    int n = arr.size();
+    bool swaped = false;
+    for(int i = 0; i< n ; i++){
+        for(int j =1 ; j<(n-i);j++){
+            if(arr[j-1]>arr[j]){
+                swap(arr[j-1],arr[j]);
+                swaped = true;
+            }
+        }
+            if(swaped != true){
+                return arr;
+            }
+    }
     return arr;
 }
 
 
 
 int main() { vector<int> arr = {23, 45, 34, 67, 6, 87, 45}; 
-    vector<int> sortedArr = selectionsort(arr); 
+    vector<int> sortedArr = bubblesort(arr); 
     cout << "Sorted array: "; 
     for (int num : sortedArr) { 
         cout << num << " ";
     } 
-    cout << endl; 
+    cout << endl;
     return 0; 
 }
