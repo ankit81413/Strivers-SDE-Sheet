22a0bd9f6a4af2e81b080806564d0de5b4a384bc
Ankit <ankitsen81413@gmail.com>
2025-09-24 16:28:30 +0530

    reverse string()

---
+++ string.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+
+string reverse_string(string str){
+    int left = 0;
+    int right = str.size()-1;
+
+    while(left < right){
+        swap(str[left],str[right]);
+        left++;
+        right--;
+    }
+    return str; 
+
+}
+
+
+int main(){
+    string str = "hellow";
+    // string reverse= reverse_string(str);
+    cout<<"reverse of "<<str<<" is "<< reverse_string(str);
+}
